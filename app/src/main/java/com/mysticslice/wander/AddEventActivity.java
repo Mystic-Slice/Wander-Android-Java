1a232e1ef0d4e539c20556003c44d1f5182c3088
Ashwath <vaashwath@gmail.com>
2021-03-07 12:33:51 +0530

    User Authentication added

--- app/src/main/java/com/mysticslice/wander/AddEventActivity.java
+++ app/src/main/java/com/mysticslice/wander/AddEventActivity.java
@@ -2,8 +2,10 @@ package com.mysticslice.wander;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,15 +18,26 @@ public class AddEventActivity extends AppCompatActivity {
     EditText eventNameView;
     EditText eventLocationView;
     EditText eventDateView;
+    EditText eventUrlView;
 
     String mEventName;
     String mEventLocation;
     String mEventDate;
+    String mCategory;
+    String mEventUrl;
+    String mDay;
     FireBaseUtils mfireBaseUtils = new FireBaseUtils();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
+
+        // implementing spinner for the categories
+        Spinner cat_spinner = (Spinner) findViewById(R.id.sp_add_categories);
+        ArrayAdapter<CharSequence> cat_adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories_array, android.R.layout.simple_spinner_item);
+        cat_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cat_spinner.setAdapter(cat_adapter);
 
         Button addEventButton = (Button) findViewById(R.id.bt_add_addevent);
         addEventButton.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +52,12 @@ public class AddEventActivity extends AppCompatActivity {
                 eventDateView = findViewById(R.id.et_add_event_date);
                 mEventDate = eventDateView.getText().toString();
 
-                Event addedEvent = new Event(mEventName,mEventLocation,mEventDate,"Drama","google.com",null);
+                eventUrlView = findViewById(R.id.et_add_event_url);
+                mEventUrl = eventUrlView.getText().toString();
+
+                mCategory = cat_spinner.getSelectedItem().toString();
+
+                Event addedEvent = new Event(mEventName,mEventLocation,mEventDate,mCategory,mEventUrl,null);
 
                 mfireBaseUtils.pushIntoDatabase(addedEvent);
                 Toast.makeText(AddEventActivity.this,"Event Added",Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/mysticslice/wander/MainActivity.java
+++ app/src/main/java/com/mysticslice/wander/MainActivity.java
@@ -1,26 +1,48 @@
 package com.mysticslice.wander;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    // CONSTANTS
+    public static final int RC_SIGN_IN = 1;
+
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
         Button eventsButton = (Button) findViewById(R.id.bt_main_events);
 
@@ -40,5 +62,42 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+
+                } else {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+
+                }
+            }
+        };
     }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
 }
